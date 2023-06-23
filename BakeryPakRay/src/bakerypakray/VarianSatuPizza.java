47d217796a825fec0099fa88654898d90e97be1e
najwamay <113868487+najwamay@users.noreply.github.com>
2023-06-23 20:45:13 +0700

    variansatupizza
    update

--- BakeryPakRay/src/bakerypakray/VarianSatuPizza.java
+++ BakeryPakRay/src/bakerypakray/VarianSatuPizza.java
@@ -14,32 +14,42 @@ public class VarianSatuPizza extends Pizza implements Varian{
 
     @Override
     public double getHargaTopFill() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        double[] hargaTopping = super.setHargaPerKomposisiTopFill(topping);
+        double[] hargaFilling = super.setHargaPerKomposisiTopFill(filling);
+        double[] hasil = new double[hargaTopping.length + hargaFilling.length];
+        System.arraycopy(hargaTopping, 0, hasil, 0, hargaTopping.length);
+        System.arraycopy(hargaFilling, 0, hasil, hargaTopping.length, hargaFilling.length);
+        double harga = 0;
+        for(int i = 0; i < hasil.length; i ++){
+            harga = harga + hasil[i];
+        }
+        return harga;
     }
 
     @Override
     public void displayCheckVarian() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        System.out.println("Varian 1 Pizza seharga Rp "+this.getHargaJualVarian());
     }
 
     @Override
     public double getModalVarian() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return this.getJumlahModalAdonanPerPcs() + this.getHargaTopFill();
     }
 
     @Override
     public int getHargaJualVarian() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int hasil = (int) (((100+this.profit)*this.getModalVarian())/100);
+        return hasil;
     }
 
     @Override
     public String[][] getTopping() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return this.topping;
     }
 
     @Override
     public String[][] getFilling() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return this.filling;
     }
     
 }

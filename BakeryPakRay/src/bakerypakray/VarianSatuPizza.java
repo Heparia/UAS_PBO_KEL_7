b594929094512d73f9a58de6685fd64626432ebd
seliags <agustinseli57@gmail.com>
2023-06-23 12:58:05 +0700

    cek
    cek

--- BakeryPakRay/src/bakerypakray/VarianSatuPizza.java
+++ BakeryPakRay/src/bakerypakray/VarianSatuPizza.java
@@ -8,8 +8,33 @@ package bakerypakray;
  *
  * @author user
  */
-public class VarianSatuPizza extends Pizza{
+public class VarianSatuPizza extends Pizza implements Varian{
     private String[][] topping = {{"Keju", "30"}, {"Sosis", "50"}, {"Smoked Beef", "50"}, {"Bawang Bombay", "30"}};
-    private String[][] filling = null;
+    private String[][] filling = {{null, null}};
+
+    @Override
+    public double getHargaTopFill() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void displayCheckVarian() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public double getModalVarian() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int getHargaJualVarian() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String[][] getGabunganTopFill() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
